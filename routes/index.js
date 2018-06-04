4bd6f9c65801f302ab22059563b55d816d4a315b
Tommy <spiel.hoelle@live.de>
2018-06-04 19:34:59 +0200

    sorting by order, comments and instructions

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ const Post = require('../models/post');
 const express = require('express')
 const router = express.Router()
 
+//public frontend where you can view the posts
 router.get('/', function(req, res) {
-    Post.find(function(err, posts) {
-        res.render('index', {
-            posts: posts
-        })
-    });
+  Post.find({}).sort('order').exec(function(err, posts) { 
+      res.render('index', {
+        posts: posts
+      })
+  });
 });
 
 module.exports = router
