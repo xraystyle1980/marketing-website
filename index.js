e87ea712af0a3d713a62048f8f71d66f745ce776
Shteyn <tarna83@mail.ru>
2018-10-23 10:41:05 +0200

    fixed styling routing in server.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config({ path: __dirname + '/.env' });
+require("dotenv").config({ path: __dirname + "/.env" });
 
-const app = require('./server.js')
+const app = require("./server.js");
 let port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
